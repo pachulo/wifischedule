c4cd4a136df426a9598d342f2655e55809721e0f
Razvan Stanga <git@razvi.ro>
2012-12-10 13:55:44 +0200

    added administrable interval to query for changes. now wifi_schedule daemon uses tmpfs as temporary storage for wifi_schedule config

--- wifischedule-luci/files/usr/lib/lua/luci/model/cbi/wifi_schedule/schedule.lua
+++ wifischedule-luci/files/usr/lib/lua/luci/model/cbi/wifi_schedule/schedule.lua
@@ -2,11 +2,11 @@
 require("luci.tools.webadmin")
 
 m = Map("wifi_schedule", translate("Wifi Schedule"),
-	translate(""))
+	translate("Wifi Schedule is a very useful app which will allow you to manage and schedule when your device Wi-Fi will turn on and off."))
 
-s = m:section(TypedSection, "wifischedule", "")
+s = m:section(TypedSection, "days", "Days of the week")
 s.addremove = false
-s.anonymous = false
+s.anonymous = true
 
 s:tab("wd1", translate("Monday"))
 s:tab("wd2", translate("Tuesday"))
@@ -25,5 +25,14 @@ for day=1,7 do
 	        	translate(""))
 	end
 end
+
+s = m:section(TypedSection, "options", "Options")
+s.addremove = false
+s.anonymous = true
+
+o = s:option(Value, "interval", translate("Interval to query for changes"), translate("In seconds"))
+o.optional = true
+o.datatype = "and(uinteger,min(30))"
+o.default = 30
 
 return m
